1b8a13ba09f2ebac8e82631dafbb9ffc51d4d8c6
CapistranoIFCE <101426741+CapistranoIFCE@users.noreply.github.com>
2022-12-16 01:41:40 -0300

    Initial Commit

---
+++ SwiftArquiteturas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftArquiteturas
+//
+//  Created by Davi Capistrano on 16/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

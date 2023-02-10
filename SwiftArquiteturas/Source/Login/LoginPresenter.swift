aa9753e4a49ae17f8b28fd05249f350b21764c53
CapistranoIFCE <101426741+CapistranoIFCE@users.noreply.github.com>
2023-02-10 06:20:17 -0300

    create Presenter

---
+++ SwiftArquiteturas/Source/Login/LoginPresenter.swift
@@ -0,0 +1,33 @@
+//
+//  LoginPresenter.swift
+//  SwiftArquiteturas
+//
+//  Created by Davi Capistrano on 10/02/23.
+//
+
+import Foundation
+
+protocol LoginPresenterDelegate {
+    func showMessage(title: String, message: String)
+    func goHome()
+}
+
+class LoginPresenter {
+    var delegate: LoginPresenterDelegate?
+
+    func login(userModel: UserModel){
+        let manager = UserManager(business: UserBusiness())
+        manager.login(email: userModel.email,
+                      password: userModel.password) {[weak self] model in
+            self?.goHome()
+
+        } failureHandler: {[weak self] error in
+            self?.delegate?.showMessage(title: "Error",
+                                        message: error!.localizedDescription)
+        }
+    }
+    func goHome() {
+        self.delegate?.goHome()
+
+    }
+}
--- SwiftArquiteturas/Source/Login/LoginViewController.swift
+++ SwiftArquiteturas/Source/Login/LoginViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    let presenter: LoginPresenter = LoginPresenter()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        presenter.delegate = self
     }
 
     @IBOutlet weak var emailTextField: UITextField!
@@ -19,11 +21,32 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
 
     @IBAction func openButtonTap(_ sender: Any) {
+        if let email = emailTextField.text,
+           let password = passwordTextField.text {
 
+            let userModel = UserModel(email: email, password: password)
+            presenter.login(userModel: userModel)
+        }
     }
     @IBAction func registerButtonTap(_ sender: Any) {
+        
 
     }
+}
 
+extension LoginViewController: LoginPresenterDelegate {
+    func goHome() {
+        let storyboard = UIStoryboard(name: "Main", bundle:nil)
+        let home = storyboard.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
 
+        home.modalPresentationStyle = .fullScreen
+        self.present(home, animated: true)
+    }
+
+    func showMessage(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alert, animated: true, completion: nil)
+    }
 }
